05ec63655ffbec27200ffee914ecf86f0978e24c
t4rcisio <tarcisiobatistaprates@gmail.com>
2022-05-13 22:35:00 -0300

    init

--- covid_daily_cases/src/index.js
+++ covid_daily_cases/src/index.js
@@ -1,8 +1,17 @@
 import Express from "express";
+import csv from "fast-csv";
+import fs from "fs";
+import path from "path";
 
 const app = Express();
 
 const port = 3000;
+
+fs.createReadStream("./database/covid.csv")
+  .pipe(csv.parse({ headers: true }))
+  .on("error", (error) => console.error(error))
+  .on("data", (row) => console.log(row))
+  .on("end", (rowCount) => console.log(`Parsed ${rowCount} rows`));
 
 app.get("/", (request, response) => {
   response.status(200).send("Backend Challenge 2021 🏅 - Covid Daily Cases");
